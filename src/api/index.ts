edf32e95b44a493f7ac40dc22eeb5e0a4bd687b6
Leonardo Overbeck da Silva <overbecksilva.leonardo@gmail.com>
2024-06-03 14:03:19 -0300

    adicionando url functions definitiva

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useUsuariosStore } from '@/stores/usuario';
 
-const url = 'http://127.0.0.1:5001/wktk-48a/us-central1/app';
+const url = 'https://us-central1-wktk-48a.cloudfunctions.net/app';
 const api = axios.create({ baseURL: url });
 
 api.interceptors.request.use((config) => {
